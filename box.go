e10f4dbf22201787f4e63d9df88667a87edf78ee
artemiy.krot <artemiy.krot@gmail.com>
2022-06-17 17:20:18 +0300

    add shape

--- box.go
+++ box.go
@@ -27,7 +27,7 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	if len(b.shapes) < b.shapesCapacity {
+	if len(b.shapes) <= b.shapesCapacity {
 		b.shapes = append(b.shapes, shape)
 	}
 	return errorBoxFull
